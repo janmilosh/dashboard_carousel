2edcd2bc99e2bc2beb1655329ca078ad98619e56
Jan Milosh <jmilosh@CMDVMOSXLT047.local>
2014-12-05 10:29:06 -0500

    Initial commit

---
+++ js/carousel.js
@@ -0,0 +1,36 @@
+$(function() {
+  var height = $(window).height();
+  var iframe = $('iframe');
+  var wrapper = $('#wrapper');
+  var left = $('#left');
+  var right = $('#right');
+  var index = 0;
+  var numberOfUrls = urls.length;
+
+  wrapper.css('height', height);
+  iframe.css('height', height);
+  left.css('height', height);
+  right.css('height', height);
+
+  
+  iframe.attr('src', urls[index]);
+
+  left.on('click', function() {
+    if (index === 0) {
+      index = urls.length - 1;
+    } else {
+      index--
+    }
+    iframe.attr('src', urls[index])
+  });
+
+  right.on('click', function() {
+    if (index === (urls.length - 1)) {
+      index = 0;
+    } else {
+      index++
+    }
+    iframe.attr('src', urls[index])
+  });
+
+});
---
+++ js/urls.js
@@ -0,0 +1,6 @@
+var urls =
+  [
+    'http://janmilosh.com',
+    'http://covermymeds.com',
+    'http://machinedesignohio.com'
+  ]
